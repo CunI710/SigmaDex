dd7b1c59c73db22400f834f0eba5def444ea13b6
Cun I <kanay.raimkulov@gmail.com>
2024-12-29 04:37:21 +0600

    Complete migration

--- DLL/Migrations/20241228221203_AddUserRoleMigration.Designer.cs
+++ DLL/Migrations/20241228221203_AddUserRoleMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(SigmaDbContext))]
-    partial class SigmaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241228221203_AddUserRoleMigration")]
+    partial class AddUserRoleMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
